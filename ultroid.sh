f2fd9290369a62b2d49c6ad396b4638f86536a4a
floramabel <139675985+floramabel@users.noreply.github.com>
2023-08-25 18:20:43 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp PR/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
+git clone https://github.com/eratron/Ultroid /root/eratron
+cp PR/.env /root/eratron/.env
+cd /root/eratron
 pip install -U -r requirements.txt
 pip3 install --no-cache-dir -r requirements.txt
 pip3 install av --no-binary av
